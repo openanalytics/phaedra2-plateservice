c543e775157471b6b3be4665aa69103d7fce119a
sberberovic <sasa.berberovic@openanalytics.eu>
2020-12-02 09:59:35 +0100

    Add OpenAPI v3 documentation /phaedra/plate-service/v3/api-docs

--- src/main/java/eu/openanalytics/phaedra/plateservice/PlateService.java
+++ src/main/java/eu/openanalytics/phaedra/plateservice/PlateService.java
@@ -11,6 +11,10 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.util.StringUtils;
 
 import eu.openanalytics.phaedra.util.jdbc.JDBCUtils;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 @SpringBootApplication
 public class PlateService {
