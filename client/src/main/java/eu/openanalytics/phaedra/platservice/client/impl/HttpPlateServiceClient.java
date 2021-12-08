fbe8c90c5121fce1a4b53a4b23765aa79d6cf71d
smarien <yTBTu7m>
2021-12-08 11:49:55 +0100

    Revert "Add client method to change calculationStatus from CalculationService."
    This reverts commit 88c4342b

--- client/src/main/java/eu/openanalytics/phaedra/platservice/client/impl/HttpPlateServiceClient.java
+++ client/src/main/java/eu/openanalytics/phaedra/platservice/client/impl/HttpPlateServiceClient.java
@@ -4,9 +4,6 @@ import eu.openanalytics.phaedra.platservice.client.PlateServiceClient;
 import eu.openanalytics.phaedra.platservice.client.exception.PlateUnresolvableException;
 import eu.openanalytics.phaedra.platservice.dto.PlateDTO;
 import eu.openanalytics.phaedra.platservice.dto.WellDTO;
-import eu.openanalytics.phaedra.platservice.enumartion.CalculationStatus;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
@@ -36,23 +33,6 @@ public class HttpPlateServiceClient implements PlateServiceClient {
             }
 
             return plate;
-        } catch (HttpClientErrorException.NotFound ex) {
-            throw new PlateUnresolvableException("Plate not found");
-        } catch (HttpClientErrorException ex) {
-            throw new PlateUnresolvableException("Error while fetching plate");
-        }
-    }
-
-    public void setCalculationStatus(long plateId, CalculationStatus status) throws PlateUnresolvableException{
-        try {
-            var plate = restTemplate.getForObject(UrlFactory.plate(plateId), PlateDTO.class);
-            if (plate == null) {
-                throw new PlateUnresolvableException("Plate could not be converted");
-            }
-            plate.setCalculationStatus(status);
-            HttpEntity<PlateDTO> requestUpdate = new HttpEntity<>(plate);
-
-            restTemplate.exchange(UrlFactory.platePut(), HttpMethod.PUT,requestUpdate,Void.class);
         } catch (HttpClientErrorException.NotFound ex) {
             throw new PlateUnresolvableException("Plate not found");
         } catch (HttpClientErrorException ex) {
