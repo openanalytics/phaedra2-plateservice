bfaac84b106973c5e1f3f353130ab0f44fa7dffe
Frederick Michielssen <fmichielssen@openanalytics.eu>
2022-01-04 17:13:11 +0100

    Added API to get experiment summaries

---
+++ dto/src/main/java/eu/openanalytics/phaedra/platservice/dto/ExperimentSummaryDTO.java
@@ -0,0 +1,10 @@
+package eu.openanalytics.phaedra.platservice.dto;
+
+public class ExperimentSummaryDTO {
+
+	public int nrPlates;
+	public int nrPlatesCalculated;
+	public int nrPlatesValidated;
+	public int nrPlatesApproved;
+
+}
