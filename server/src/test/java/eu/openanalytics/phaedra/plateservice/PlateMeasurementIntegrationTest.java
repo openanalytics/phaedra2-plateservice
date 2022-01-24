4f3083d8ddc8eb1466b6f306ecbbd5bdc0beb274
sberberovic <sasa.berberovic@openanalytics.eu>
2022-01-24 16:40:46 +0100

    Update and fix test failures

--- server/src/test/java/eu/openanalytics/phaedra/plateservice/PlateMeasurementIntegrationTest.java
+++ server/src/test/java/eu/openanalytics/phaedra/plateservice/PlateMeasurementIntegrationTest.java
@@ -1,25 +1,33 @@
 package eu.openanalytics.phaedra.plateservice;
 
-public class PlateMeasurementIntegrationTest {
+import eu.openanalytics.phaedra.plateservice.support.AbstractIntegrationTest;
+import eu.openanalytics.phaedra.platservice.dto.PlateMeasurementDTO;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
 
-//    @Test
-//    public void setActivePlateMeasurement() throws Exception {
-//        var res1 = performRequest(get("/plate/2000/measurement/1000"), HttpStatus.OK, PlateMeasurementDTO.class);
-//        Assertions.assertTrue(res1.getMeasurementId().equals(1000L));
-//        Assertions.assertTrue(res1.getActive());
-//
-//        var res2 = performRequest(get("/plate/2000/measurement/2000"), HttpStatus.OK, PlateMeasurementDTO.class);
-//        Assertions.assertTrue(res2.getMeasurementId().equals(2000L));
-//        Assertions.assertFalse(res2.getActive());
-//
-//        performRequest(put("/plate/2000/measurement/2000", null), HttpStatus.OK, PlateMeasurementDTO.class);
-//
-//        res1 = performRequest(get("/plate/2000/measurement/1000"), HttpStatus.OK, PlateMeasurementDTO.class);
-//        Assertions.assertTrue(res1.getMeasurementId().equals(1000L));
-//        Assertions.assertFalse(res1.getActive());
-//
-//        res2 = performRequest(get("/plate/2000/measurement/2000"), HttpStatus.OK, PlateMeasurementDTO.class);
-//        Assertions.assertTrue(res2.getMeasurementId().equals(2000L));
-//        Assertions.assertTrue(res2.getActive());
-//    }
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+public class PlateMeasurementIntegrationTest extends AbstractIntegrationTest {
+
+    @Test
+    public void setActivePlateMeasurement() throws Exception {
+        var res1 = performRequest(get("/plate/2000/measurement/1000"), HttpStatus.OK, PlateMeasurementDTO.class);
+        Assertions.assertTrue(res1.getMeasurementId().equals(1000L));
+        Assertions.assertTrue(res1.getActive());
+
+        var res2 = performRequest(get("/plate/2000/measurement/2000"), HttpStatus.OK, PlateMeasurementDTO.class);
+        Assertions.assertTrue(res2.getMeasurementId().equals(2000L));
+        Assertions.assertFalse(res2.getActive());
+
+        performRequest(put("/plate/2000/measurement/2000", null), HttpStatus.OK, PlateMeasurementDTO.class);
+
+        res1 = performRequest(get("/plate/2000/measurement/1000"), HttpStatus.OK, PlateMeasurementDTO.class);
+        Assertions.assertTrue(res1.getMeasurementId().equals(1000L));
+        Assertions.assertFalse(res1.getActive());
+
+        res2 = performRequest(get("/plate/2000/measurement/2000"), HttpStatus.OK, PlateMeasurementDTO.class);
+        Assertions.assertTrue(res2.getMeasurementId().equals(2000L));
+        Assertions.assertTrue(res2.getActive());
+    }
 }
