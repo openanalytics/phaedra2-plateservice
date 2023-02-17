ca5aaf3a9fb03c3039f5054f9391ff34813abee0
sberberovic <sasa.berberovic@openanalytics.eu>
2023-02-17 13:47:03 +0100

    Fix typo

--- server/src/main/java/eu/openanalytics/phaedra/plateservice/service/KafkaConsumerService.java
+++ server/src/main/java/eu/openanalytics/phaedra/plateservice/service/KafkaConsumerService.java
@@ -26,7 +26,6 @@ import eu.openanalytics.phaedra.plateservice.model.Plate;
 import eu.openanalytics.phaedra.plateservice.repository.PlateRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.handler.annotation.Header;
@@ -47,7 +46,7 @@ public class KafkaConsumerService {
     //TODO: Configure kafka consumer security
     @KafkaListener(topics = KafkaConfig.PLATE_TOPIC, groupId = "plate-service", filter = "keyFilterStrategy")
     public void onUpdatePlateCalculationStatus(PlateCalculationStatusDTO plateCalcStatusDTO, @Header(KafkaHeaders.RECEIVED_KEY) String msgKey) {
-        logger.info("plaste-service: receiving message on event " + msgKey);
+        logger.info("plate-service: receiving message on event " + msgKey);
         if (msgKey.equals(KafkaConfig.PLATE_CALCULATION_EVENT)) {
             Optional<Plate> result = plateRepository.findById(plateCalcStatusDTO.getPlateId());
             if (result.isPresent()) {
