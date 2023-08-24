e9bcdfc30a5c8cbb0408a158b481157de38d1131
sberberovic <sasa.berberovic@openanalytics.eu>
2023-08-24 17:01:41 +0200

    update: update DataScalar

--- server/src/main/java/eu/openanalytics/phaedra/plateservice/scalars/DateScalar.java
+++ server/src/main/java/eu/openanalytics/phaedra/plateservice/scalars/DateScalar.java
@@ -36,7 +36,7 @@ public class DateScalar implements Coercing<Date, String> {
     @Override
     public String serialize(final Object dataFetcherResult) {
         if (dataFetcherResult instanceof Date) {
-            return DateFormatUtils.format((Date) dataFetcherResult, "yyyy-MM-dd HH:mm:ss.SS");
+            return DateFormatUtils.format((Date) dataFetcherResult, "yyyy-MM-ddTHH:mm:ss.SS");
         } else {
             throw new CoercingSerializeException("Expected a LocalDate object.");
         }
@@ -46,7 +46,7 @@ public class DateScalar implements Coercing<Date, String> {
     public Date parseValue(final Object input) {
         try {
             if (input instanceof String) {
-                return DateUtils.parseDate((String) input, "yyyy-MM-dd HH:mm:ss.SS");
+                return DateUtils.parseDate((String) input, "yyyy-MM-ddTHH:mm:ss.SS");
             } else {
                 throw new CoercingParseValueException("Expected a String");
             }
@@ -59,7 +59,7 @@ public class DateScalar implements Coercing<Date, String> {
     public Date parseLiteral(final Object input) {
         if (input instanceof StringValue) {
             try {
-                return DateUtils.parseDate(((StringValue) input).getValue(), "yyyy-MM-dd HH:mm:ss.SS");
+                return DateUtils.parseDate(((StringValue) input).getValue(), "yyyy-MM-ddTHH:mm:ss.SS");
             } catch (ParseException e) {
                 throw new CoercingParseLiteralException(e);
             }
