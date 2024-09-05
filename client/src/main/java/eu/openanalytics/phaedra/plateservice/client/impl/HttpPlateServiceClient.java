b16b575602694c3b72fd457d5d7305a643d93634
sberberovic <sasa.berberovic@openanalytics.eu>
2024-09-05 17:16:24 +0200

    fix: missing implementation abstract method

--- client/src/main/java/eu/openanalytics/phaedra/plateservice/client/impl/HttpPlateServiceClient.java
+++ client/src/main/java/eu/openanalytics/phaedra/plateservice/client/impl/HttpPlateServiceClient.java
@@ -150,6 +150,11 @@ public class HttpPlateServiceClient implements PlateServiceClient {
     }
 
     @Override
+    public List<WellDTO> getNWells(int n) throws UnresolvableObjectException {
+        return List.of();
+    }
+
+    @Override
     public List<PlateDTO> getPlatesByBarcode(String barcode) {
     	var response = restTemplate.exchange(urlFactory.platesByBarcode(barcode), HttpMethod.GET, new HttpEntity<String>(makeHttpHeaders()), PlateDTO[].class);
     	return Arrays.stream(response.getBody()).toList();
